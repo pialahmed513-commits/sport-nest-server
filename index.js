7c3cca7dcccc3089f93d05e9954f75a06992ccfb
pialahmed513-commits <pialahmed513@gmail.com>
2026-05-21 01:25:43 +0600

    Removed trailing slashes for CORS fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", 
-      "https://sport-nest-server-pi.vercel.app/" 
+      "https://sport-nest-server-pi.vercel.app" 
     ],
     credentials: true,
   })
